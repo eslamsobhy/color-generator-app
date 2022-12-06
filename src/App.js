e52b495da73d66cd08ba014b71b0e43628eff78e
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-12-06 23:41:59 +0200

    captured each color

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import Values from "values.js";
 function App() {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault(); //to prevent refreshing when submitting!
     try {
       let colors = new Values(color).all(10);
-      console.log(colors);
+      setList(colors);
     } catch (error) {
       console.log(error);
       setError(true);
@@ -36,7 +37,10 @@ function App() {
         </form>
       </section>
       <section className="colors">
-        <h4>list goes here!</h4>
+        {list.map((color, index) => {
+          console.log(color);
+          return <SingleColor key={index} {...color}></SingleColor>;
+        })}
       </section>
     </>
   );
