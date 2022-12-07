8c2323b5b450b38d309536798144a271bbca0317
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-12-07 16:01:43 +0200

    displayed colors

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -2,7 +2,15 @@ import React, { useState, useEffect } from "react";
 import rgbToHex from "./utils";
 
 const SingleColor = ({ rgb, weight }) => {
-  return <h4>single color</h4>;
+  const bgc = rgb.join(",");
+  const hex = rgbToHex(...rgb);
+
+  return (
+    <article className="color" style={{ backgroundColor: `rgb(${bgc})` }}>
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hex}</p>
+    </article>
+  );
 };
 
 export default SingleColor;
