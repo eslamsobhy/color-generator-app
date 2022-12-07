ea4b2bd2980cadb5152bb2f8ddab5d9d06411361
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-12-07 17:18:08 +0200

    copied the hex value to the clipboard

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -2,16 +2,24 @@ import React, { useState, useEffect } from "react";
 import rgbToHex from "./utils";
 
 const SingleColor = ({ rgb, weight, hexColor, index }) => {
+  const [alert, setAlert] = useState(false);
+
   const bgc = rgb.join(",");
   // const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
 
   return (
     <article
       className={`color ${index > 10 && "color-light"}`}
       style={{ backgroundColor: `rgb(${bgc})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
     >
       <p className="percent-value">{weight}%</p>
-      <p className="color-value">#{hexColor}</p>
+      <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">copied to clipboard!</p>}
     </article>
   );
 };
